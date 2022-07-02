6055785113e69dad6e703d575d59e91880592d70
ArifBabayev05 <arifrb@code.edu.az>
2022-07-02 16:05:11 +0400

    Create Events 80%

---
+++ Business/Repository/EventRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Services;
+using DAL.Data;
+using DAL.Models;
+using Exceptions.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Repository
+{
+    public class EventRepository : IEventService
+    {
+        private readonly AppDbContext _context;
+        public EventRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Event> Get(int? id)
+        {
+            if (id is null)
+            {
+                throw new ArgumentNullException("Id");
+            }
+            var data = await _context.Events.Where(n => n.Id == id && !n.isDeleted)
+                                             .Include(n => n.City)
+                                             .FirstOrDefaultAsync();
+            if (data is null)
+            {
+                throw new EntityIsNullException();
+            }
+            return data;
+        }
+
+        public async Task<List<Event>> GetAll()
+        {
+            var data = await _context.Events.Where(n => !n.isDeleted).Include(n => n.City).ToListAsync();
+            if (data is null)
+            {
+                throw new EntityIsNullException();
+            }
+            return data;
+        }
+
+        public async Task Create(Event entity)
+        {
+            entity.CreatedDate = DateTime.UtcNow.AddHours(4);
+            await _context.Events.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(int id, Event entity)
+        {
+            var data = await Get(id);
+            data.Title = entity.Title;
+            data.City = entity.City;
+            data.UpdatedDate = DateTime.UtcNow.AddHours(4);
+
+            await _context.SaveChangesAsync();
+
+        }
+        public async Task Delete(int id)
+        {
+            var data = await Get(id);
+            data.isDeleted = true;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ DAL/Migrations/20220702111058_EventMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class EventMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    isDeleted = table.Column<bool>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    CityId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CityId",
+                table: "Events",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
